f778534a2a98a9358ad42b3ce9c8e164389f5def
jeongjiwooon <jjwanm@naver.com>
2023-05-23 16:03:27 +0900

    commit

--- src/Onehandcardgame.java
+++ src/Onehandcardgame.java
@@ -9,26 +9,26 @@ public class Onehandcardgame
         int C1 = 1;
         int C2 = 2;
 
-        System.out.println(A+""+B+""+C);
+        System.out.println("빈 공간 A, B, C를 만든다. "+A+""+B+""+C);
 
         A = C1;
         C = C2;
 
-        System.out.println(A+""+B+""+C);
+        System.out.println("A에 숫자 1, C에 숫자 2를 놓는다. "+A+""+B+""+C);
 
         B = C2;
         C = 0;
 
-        System.out.println(A+""+B+""+C);
+        System.out.println("C에 있는 숫자 2를 B로 옮긴다. "+A+""+B+""+C);
 
         C = C1;
         A = 0;
 
-        System.out.println(A+""+B+""+C);
+        System.out.println("A에 있는 숫자 1을 C로 옮긴다. "+A+""+B+""+C);
 
         A = C2;
         B = 0;
 
-        System.out.println(A+""+B+""+C);
+        System.out.println("B에 있는 숫자 2를 A로 옮긴다. "+A+""+B+""+C);
     }
 }
