3491ec01cad415fdc790cf9518cc7fb91fc5db77
jeongjiwooon <jjwanm@naver.com>
2023-05-24 15:21:46 +0900

    commit

---
+++ src/LoopWithMark.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class LoopWithMark
+{
+    public static void main(String[] args)
+    {
+        for (int line = 1; line < 5; line++)
+        {
+            System.out.println("for "+line);
+        }
+    }  
+}
