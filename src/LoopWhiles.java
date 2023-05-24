c5d290db563472f20eb029260fb5aee18ed8c582
jeongjiwooon <jjwanm@naver.com>
2023-05-24 11:30:54 +0900

    commit

--- src/LoopWhiles.java
+++ src/LoopWhiles.java
@@ -7,13 +7,25 @@ public class LoopWhiles
         //     // code block to be executed
         // }
 
-            int first = 5;
-            int second = 10;
-            while (first < second)
+            // int first = 5;
+            // int second = 10;
+            // while (first < second)
+            // {
+            //     System.out.println(first);
+            //     first = first + 2;
+            // }
+            int num = 2;
+        while (num < 10)
+        {
+            int multiply = 1;
+            while (multiply < 10)
             {
-                System.out.println(first);
-                first = first + 2;
+                System.out.print(num*multiply+" ");
+                multiply = multiply + 1;
             }
+            System.out.print(" / ");
+            num = num + 1;
+        }
 
         // return 0;
     }
