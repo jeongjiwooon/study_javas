2d271d41b3b2cde688f4fe9d6b2b4a7873a35330
jeongjiwooon <jjwanm@naver.com>
2023-05-24 15:02:59 +0900

    commit

---
+++ src/LoopFors.java
@@ -0,0 +1,16 @@
+public class LoopFors
+{
+    public static void main(String[] args)
+    {   
+        int second = 10;
+        for (int first = 5; first < second; first = first + 2)
+        {
+            System.out.println(first);
+            // code block to be executed
+        }
+
+        System.out.println();
+
+        // //   return 0;
+    }    
+}
