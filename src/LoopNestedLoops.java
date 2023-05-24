c93d12aff3b7c9f66ce8353fb5d9e8367e9002ab
jeongjiwooon <jjwanm@naver.com>
2023-05-24 16:23:32 +0900

    commit

--- src/LoopNestedLoops.java
+++ src/LoopNestedLoops.java
@@ -5,6 +5,24 @@ public class LoopNestedLoops
         for (int first = 1; first < 5; first = first + 1)
         {
             System.out.println("for " +  first);
+            for (int second = 1; second < 5; second = second + 1)
+            {
+                if (second <= first)
+                {
+                    System.out.print("_?");   
+                }
+                else
+                {
+                 break;
+                }
+            }
+            System.out.println();
+        }
+        System.out.println("end");
+
+        for (int first = 1; first < 5; first = first + 1)
+        {
+            System.out.println("for " +  first);
             for (int second = 1; second <= first; second = second + 1)
             {
                 System.out.print("_?");
