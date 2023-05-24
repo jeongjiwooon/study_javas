8619a42e92f7fed9eb606c3c763d4bb774abdfef
jeongjiwooon <jjwanm@naver.com>
2023-05-24 18:21:33 +0900

    commit

---
+++ src/Arrays.java
@@ -0,0 +1,12 @@
+public class Arrays
+{
+    public static void main(String[] args)
+    {
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        for (int i = 0; i < 4; i = i + 1)
+        {
+            System.out.println(cars[i]);
+        }
+            // return 0;
+    }
+}
