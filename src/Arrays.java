de741194d23c649ba70d6354871eb482213c1875
jeongjiwooon <jjwanm@naver.com>
2023-05-25 09:23:19 +0900

    commit

--- src/Arrays.java
+++ src/Arrays.java
@@ -3,9 +3,9 @@ public class Arrays
     public static void main(String[] args)
     {
         String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
-        for (int i = 0; i < 4; i = i + 1)
+        for (int num = 0; num < 4; num = num + 1)
         {
-            System.out.println(cars[i]);
+            System.out.println(cars[num]);
         }
             // return 0;
     }
---
+++ src/PersonalPractice.java
@@ -0,0 +1,7 @@
+public class PersonalPractice
+{
+    public static void main()
+    {
+        System.out.println(3+5);
+    }
+}
