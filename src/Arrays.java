6f3fad6a37f14673f4e18737dccc7a4d9137e006
jeongjiwooon <jjwanm@naver.com>
2023-05-25 10:15:25 +0900

    commit

--- src/Arrays.java
+++ src/Arrays.java
@@ -3,9 +3,10 @@ public class Arrays
     public static void main(String[] args)
     {
         String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
-        for (int num = 0; num < 4; num = num + 1)
+        System.out.println(cars.length);
+        for (int first = 0; first < 4; first = first + 1)
         {
-            System.out.print(cars[num]+" "); // Volvo BMW Ford Mazda
+            System.out.print(cars[first]+" "); // Volvo BMW Ford Mazda
         }
             // return 0;
     }
