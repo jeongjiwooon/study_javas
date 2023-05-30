47475ac5185f3f56812b94c9bfe0367f0af5f2ff
jeongjiwooon <jjwanm@naver.com>
2023-05-30 18:29:54 +0900

    commit

--- src/MethodsIfs.java
+++ src/MethodsIfs.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class MethodsIfs
 {
     public int day(int time)
@@ -6,20 +8,25 @@ public class MethodsIfs
 
         try
         {
-            while (time != 0)
+            while (time <= 24 || time >= 0)
             {
-                time = time + 6;
                 if (time < 12)
                 {
-                    System.out.println("오전입니다.");
+                    System.out.println(time+"시는 오전입니다.");
+                    time = time + 6;
+                    System.out.println("입력한 시간으로부터 6시간이 흘러서 "+time+"시입니다.");
                 }
                 else if (time < 18)
                 {
-                    System.out.println("오후입니다.");
+                    System.out.println(time+"시는 오후입니다.");
+                    time = time + 6;
+                    System.out.println("입력한 시간으로부터 6시간이 흘러서 "+time+"시입니다.");
                 }
                 else
                 {
-                    System.out.println("밤입니다.");
+                    System.out.println(time+"시는 밤입니다.");
+                    time = time + 6;
+                    System.out.println("입력한 시간으로부터 6시간이 흘러서 "+time+"시입니다.");
                     break;
                 }
             }
@@ -35,10 +42,13 @@ public class MethodsIfs
     {
         try
         {
-            int time = 1;
+            Scanner myObj = new Scanner(System.in);
+            System.out.println("시간은 0시부터 24시까지만 입력 가능합니다.");
+            System.out.println("시간을 입력하시오.: ");
+            int time = myObj.nextInt();
             MethodsIfs methodifs = new MethodsIfs();
-            int returnResult = methodifs.day(time);
-            System.out.println(returnResult);
+            int result = methodifs.day(time);
+            System.out.println(result);
         }
         catch (Exception e)
         {
