673a83779ba5a42ef0d7d3db3f1156156f4c9617
jeongjiwooon <jjwanm@naver.com>
2023-05-30 14:12:19 +0900

    commit

--- src/MethodBasics.java
+++ src/MethodBasics.java
@@ -1,30 +1,18 @@
 public class MethodBasics
 {
-    // x에 + 1
-    // public int F(int x)
-    // {
-    //     int result = x + 1;
-        
-    //     return result;
-    // }
-
     public int F(int x)
     {
         int result = x + 1;
         return result;
     }
-
     public static void main(String[] args)
     {
         try
         {
-            // int x = 4;
-            int x = 8;
+            int x = 9;
             int y;
             MethodBasics methodBasics = new MethodBasics();
             y = methodBasics.F(x);
-            // y = methodBasics.F(x);
-            // y = y + 1; // 변수에 + 1
             System.out.println(y);
         }
 
