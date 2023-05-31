0097772a8228072cb6f43058a35004a0a5a4af7c
jeongjiwooon <jjwanm@naver.com>
2023-05-31 11:45:36 +0900

    commit

--- src/GitConflicts.java
+++ src/GitConflicts.java
@@ -6,9 +6,6 @@ public class GitConflicts
         {
             int first = 0;
             first = first + 1;
-            int second = 1;
-            // int first = 0;
-            second = first + 1;
         }
         catch (Exception e)
         {
