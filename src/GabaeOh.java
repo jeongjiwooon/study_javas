15780bfc8b20db0da02030ab0641900b0c583e24
GabaeOh <rkqo4577@gmail.com>
2023-05-31 12:29:28 +0900

    가배

---
+++ src/GabaeOh.java
@@ -0,0 +1,10 @@
+public class GabaeOh {
+    public static void main(String[] args) {
+        try {
+            System.out.println("오가배");
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+        System.out.println("");
+    }
+}
