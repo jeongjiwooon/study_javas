a34241719e91c99cff577765eab19f0fb551c5dd
jeongjiwooon <jjwanm@naver.com>
2023-06-12 12:43:32 +0900

    commit

--- src/ConnectDBs.java
+++ src/ConnectDBs.java
@@ -1,25 +1,71 @@
 import java.sql.*;
 
-public class ConnectDBs
-{
-    public static void main(String[] args)
-    {
-        try
-        {
+public class ConnectDBs {
+    public static void main(String[] args) {
+        try {
             // - MySQL workbench 실행 : JDBC
-        // - User/password와 접속 IP:port 접속
-        String url = "jdbc:mysql://localhost:3306/db_cars";
-        String user = "root";
-        String password = "12345678";
+            // - User/password와 접속 IP:port 접속
+            String url = "jdbc:mysql://127.0.0.1:3306/db_cars";
+            String user = "root";
+            String password = "12345678";
 
-        Connection connection = DriverManager.getConnection(url, user, password);
+            Connection connection = DriverManager.getConnection(url, user, password);
             System.out.println("DB연결 성공\n");
 
+            // - query Edit
+            Statement statement = connection.createStatement();
+            String query = "SELECT * FROM factorys";
+            ResultSet resultSet = statement.executeQuery(query);
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString("COMPANY_ID") + resultSet.getString("COMPANY"));
+            }
+
+            // SELECT COUNT(*) AS CNT FROM factorys;
+            query = "SELECT COUNT(*) AS CNT FROM factorys";
+            resultSet = statement.executeQuery(query);
+            int totalCount = 0;
+            while (resultSet.next()) {
+                System.out.println(resultSet.getInt("CNT"));
+                totalCount = resultSet.getInt("CNT");
+            }
+
+            /*
+            INSERT INTO factorys
+            (COMPANY_ID, COMPANY) 
+            VALUE 
+            ('CAR-01', 'AUDI');
+            */
+            String companyId = "CAR-01";
+            String company = "AUDI" ;
+            query = "INSERT INTO factorys " +
+                    "(COMPANY_ID, COMPANY) " +
+                    " VALUE " +
+                    "('"+companyId+"', '"+company+"') ";
+            
+            int count = statement.executeUpdate(query);
             System.out.println();
-        }
-        catch (Exception e)
-        {
+
+            companyId = "CAR-01";
+            company = "페라리";
+            query = "UPDATE factorys " +
+                    "SET COMPANY = ('"+company+"') " +
+                    "WHERE COMPANY_ID = ('"+companyId+"') ";
+            count = statement.executeUpdate(query);
+
+            System.out.println();
+
+            companyId = "CAR-01";
+            query = "DELETE FROM factorys " +
+                    "WHERE COMPANY_ID = ('"+companyId+"') ";
+
+            count = statement.executeUpdate(query);
+
+            System.out.println();
+
+        } catch (Exception e) {
             // TODO: handle exception
+            System.out.println(e.getMessage());
         }
+        System.out.println();
     }
 }
