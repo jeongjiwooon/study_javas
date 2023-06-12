8ca6c3e17132c915fa573af6134c838b926e4950
jeongjiwooon <jjwanm@naver.com>
2023-06-12 17:10:57 +0900

    commit

---
+++ src/cars/FactoryDMLs.java
@@ -0,0 +1,20 @@
+package cars;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class FactoryDMLs
+{
+    public ResultSet selectStatements(Statement statement, String query) throws SQLException
+    {
+        ResultSet resultSet = statement.executeQuery(query);
+        return resultSet;
+    }
+
+    public int insertStatements(Statement statement, String query) throws SQLException
+    {
+        int count = statement.executeUpdate(query);
+        return 0;
+    }
+}
